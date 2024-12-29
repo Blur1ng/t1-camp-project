01c152e26938b2a1e6993393e55123f68590b6b0
Vladimir Utratenko <vutratenko@gmail.com>
2024-12-29 22:01:46 +0300

    Add PostgreSQL Database support

--- internal/datatypes.go
+++ internal/datatypes.go
@@ -1,5 +1,7 @@
 package internal
 
+import "time"
+
 type API struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
@@ -7,4 +9,21 @@ type API struct {
 
 type apiHandler struct {
 	api *API
+}
+
+type postgresSamples struct {
+	ID      int
+	Message string
+}
+
+type rabbitMQSamples struct {
+	ID        int64
+	Timestamp time.Time
+	Message   string
+}
+
+type kafkaSamples struct {
+	ID        int64
+	Timestamp time.Time
+	Message   string
 }
--- internal/http.go
+++ internal/http.go
@@ -10,9 +10,16 @@ import (
 )
 
 func RunServer() {
+	//Initialize database
+	go func() {
+		initDB()
+	}()
+
+	//Run webserver
 	log.Info().Msgf("Running PIG Dummy Service on port %s", viper.GetString("Port"))
 	r := mux.NewRouter()
 	r.HandleFunc("/internal/healthz", healthcheck)
+	r.HandleFunc("/database", getDBData)
 	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("resources/"))))
 	r.Use(loggingMiddleware)
 	http.Handle("/", r)
@@ -29,6 +36,44 @@ func loggingMiddleware(next http.Handler) http.Handler {
 		// Call the next handler, which can be another middleware in the chain, or the final handler.
 		next.ServeHTTP(w, r)
 	})
+}
+
+func getDBData(w http.ResponseWriter, r *http.Request) {
+	var response API
+	w.Header().Add("Content-Type", "application/json")
+	w.Header().Set("Server", "PIG Dummy Service")
+
+	var data postgresSamples
+
+	if len(viper.GetString("Database")) > 1 {
+		data = getPGData()
+	} else {
+		data = postgresSamples{
+			ID:      0,
+			Message: "NO DATA IN DB",
+		}
+	}
+
+	if r.Method != http.MethodGet {
+		response = API{
+			Code:    405,
+			Message: "Method not allowed",
+		}
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	} else {
+		response = API{
+			Code:    200,
+			Message: data.Message,
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+	js, err := json.Marshal(response)
+	if err != nil {
+		log.Debug().Msg(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(js)
 }
 
 func healthcheck(w http.ResponseWriter, r *http.Request) {
